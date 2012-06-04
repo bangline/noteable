a42ccdefd7f01948155db27e085f84039d83ac89
Dave Kennedy <david@bangline.co.uk>
2012-06-05 00:39:40 +0100

    Simple view for the app

--- public/javascripts/notable.js
+++ public/javascripts/notable.js
@@ -1,7 +1,14 @@
 (function($) {
 
   var Note = Backbone.Model.extend({});
+  var NoteView = Backbone.View.extend({
+    render: function(){
+      var html = '<h2>' + this.model.get('title') + '</h2>';
+      $(this.el).html(html);
+    }
+  });
 
   window.note = new Note({ title: 'Learning Backbone', created_at: new Date() });
+  window.noteView = new NoteView({ model: note });
 
 })(jQuery);
