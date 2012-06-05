930a4c3f92536e6efcde5948d5223a4168d9f7cb
Dave Kennedy <david@bangline.co.uk>
2012-06-06 00:37:54 +0100

    push the model view to the page

--- public/javascripts/notable.js
+++ public/javascripts/notable.js
@@ -1,8 +1,9 @@
-(function($) {
+$(function() {
 
   var Note = Backbone.Model.extend({});
 
   var NoteView = Backbone.View.extend({
+    tagName: 'article',
     className: 'note-view',
 
     template: _.template("<h2><%= title %></h2>"),
@@ -10,10 +11,13 @@
     render: function(){
       var attr = this.model.toJSON();
       this.$el.html(this.template(attr));
+      return this;
     }
   });
 
   window.note = new Note({ title: 'Learning Backbone', created_at: new Date() });
+  
   window.noteView = new NoteView({ model: note });
+  $('#app').html(window.noteView.render().el);
 
-})(jQuery);
+});
