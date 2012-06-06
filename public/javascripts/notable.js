26fb58a8272a7fabc51c0117582f7969e04f45ec
Dave Kennedy <david@bangline.co.uk>
2012-06-06 09:57:41 +0100

    Add starred event to model and view

--- public/javascripts/notable.js
+++ public/javascripts/notable.js
@@ -1,9 +1,20 @@
 $(function() {
 
   var Note = Backbone.Model.extend({
+
     defaults: {
-      content: 'notes to be rendered',
-      created_at: function(){ return new Date() }
+      content: 'Edit your text here',
+      title: 'New Note',
+      created_at: new Date().toString('dd/MM/yy HH:mm:ss'),
+      starred: false
+    },
+
+    toggleAsFavorite: function() {
+      if( this.get('starred') ) {
+        this.set('starred', false)
+      } else {
+        this.set('starred', true)
+      }
     }
   });
 
@@ -11,12 +22,36 @@ $(function() {
     tagName: 'article',
     className: 'note-view span4',
 
-    template: _.template("<div><h2><%= title %></h2><span><%= created_at().toString('dd/MM/yy HH:mm:ss') %></span><p><%= content %></p></div>"),
+    initialize: function(){
+      this.model.on('change', this.render, this);
+    },
+
+    template: _.template("\
+      <div> \
+        <h2><%= title %></h2> \
+        <span><%= created_at %></span> \
+        <p><%= content %></p> \
+        <div class='actions'> \
+          <a href='#' class='favorite'><% starred ? print('Un-favorite') : print('Favorite') %></a> \
+          <a href='#' class='save'>Save</a> \
+          <a href='#' class='delete'>Delete</a> \
+        </div> \
+      </div>"),
+
+    events: {
+      'click .favorite': 'toggleAsFavorite'
+    },
 
     render: function(){
       var attr = this.model.toJSON();
       this.$el.html(this.template(attr));
       return this;
+    },
+
+    toggleAsFavorite: function() {
+      this.model.toggleAsFavorite();
+      this.model.get('starred') ? this.$el.addClass('favorited') : this.$el.removeClass('favorited');
+      return false;
     }
   });
 
